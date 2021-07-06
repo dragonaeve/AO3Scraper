e5edee174dce0106e44bd8af7694bd6451fe5a95
Anna <dragonaeve@gmail.com>
2021-07-05 17:25:50 -0700

    yoi restart bash script wip, fix comment unicode error, fix comment url redirect, fix <50 kudos error

--- ao3_get_fanfics.py
+++ ao3_get_fanfics.py
@@ -42,6 +42,7 @@ from unidecode import unidecode
 # seconds to wait between page requests
 delay = 5
 
+
 def get_tag_info(category, meta):
 	'''
 	given a category and a 'work meta group, returns a list of tags (eg, 'rating' -> 'explicit')
@@ -50,36 +51,42 @@ def get_tag_info(category, meta):
 		tag_list = meta.find("dd", class_=str(category) + ' tags').find_all(class_="tag")
 	except AttributeError as e:
 		return []
-	return [unidecode(result.text) for result in tag_list] 
-	
+	return [unidecode(result.text) for result in tag_list]
+
+
 def get_stats(meta):
 	'''
 	returns a list of  
 	language, published, status, date status, words, chapters, comments, kudos, bookmarks, hits
 	'''
-	categories = ['language', 'published', 'status', 'words', 'chapters', 'comments', 'kudos', 'bookmarks', 'hits'] 
+	categories = ['language', 'published', 'status', 'words', 'chapters', 'comments', 'kudos', 'bookmarks', 'hits']
 
 	stats = list(map(lambda category: meta.find("dd", class_=category), categories))
 
 	if not stats[2]:
-		stats[2] = stats[1] #no explicit completed field -- one shot
-	try:		
+		stats[2] = stats[1]  # no explicit completed field -- one shot
+	try:
 		stats = [unidecode(stat.text) for stat in stats]
-	except AttributeError as e: #for some reason, AO3 sometimes miss stat tags (like hits)
+	except AttributeError as e:  # for some reason, AO3 sometimes miss stat tags (like hits)
 		new_stats = []
 		for stat in stats:
-			if stat: new_stats.append(unidecode(stat.text))
-			else: new_stats.append('null')
+			if stat:
+				new_stats.append(unidecode(stat.text))
+			else:
+				new_stats.append('null')
 		stats = new_stats
 
-	stats[0] = stats[0].rstrip().lstrip() #language has weird whitespace characters
-	#add a custom completed/updated field
-	status  = meta.find("dt", class_="status")
-	if not status: status = 'Completed' 
-	else: status = status.text.strip(':')
+	stats[0] = stats[0].rstrip().lstrip()  # language has weird whitespace characters
+	# add a custom completed/updated field
+	status = meta.find("dt", class_="status")
+	if not status:
+		status = 'Completed'
+	else:
+		status = status.text.strip(':')
 	stats.insert(2, status)
 
-	return stats      
+	return stats
+
 
 def get_tags(meta):
 	'''
@@ -89,9 +96,10 @@ def get_tags(meta):
 	tags = ['rating', 'category', 'fandom', 'relationship', 'character', 'freeform']
 	return list(map(lambda tag: get_tag_info(tag, meta), tags))
 
+
 # get kudos
 def get_all_kudos(url, header_info):
-	headers = {'user-agent' : header_info}
+	headers = {'user-agent': header_info}
 	all_kudos = []
 
 	req = requests.get(url, headers=headers)
@@ -100,27 +108,28 @@ def get_all_kudos(url, header_info):
 
 	# find all pages
 	if (soup.find('ol', class_='pagination actions')):
-		pages = soup.find('ol', class_='pagination actions').findChildren("li" , recursive=False)
+		pages = soup.find('ol', class_='pagination actions').findChildren("li", recursive=False)
 		max_pages = int(pages[-2].contents[0].contents[0])
 		count = 1
 
 		while count <= max_pages:
-			print ('count', count)
+			print('count', count)
 			# extract each bookmark per user
 			tags = soup.find("p", class_="kudos")
 			all_kudos += get_users(tags, 'kudo')
 
 			# next page
-			count+=1
-			req = requests.get(url+'?page='+str(count), headers=headers)
+			count += 1
+			req = requests.get(url + '?page=' + str(count), headers=headers)
 			src = req.text
 			soup = BeautifulSoup(src, 'html.parser')
 	else:
 		# single page of kudos
 		tags = soup.find("p", class_="kudos")
 		all_kudos += get_users(tags, 'kudo')
-		
+
 	return all_kudos
+
 
 # get author(s)
 def get_authors(meta):
@@ -133,10 +142,11 @@ def get_authors(meta):
 
 	return authors
 
+
 # get bookmarks by page
 def get_bookmarks(url, header_info):
 	bookmarks = []
-	headers = {'user-agent' : header_info}
+	headers = {'user-agent': header_info}
 
 	req = requests.get(url, headers=headers)
 	src = req.text
@@ -149,10 +159,10 @@ def get_bookmarks(url, header_info):
 
 	# find all pages
 	if (soup.find('ol', class_='pagination actions')):
-		pages = soup.find('ol', class_='pagination actions').findChildren("li" , recursive=False)
+		pages = soup.find('ol', class_='pagination actions').findChildren("li", recursive=False)
 		max_pages = int(pages[-2].contents[0].contents[0])
 		count = 1
-	
+
 		sys.stdout.write('(' + str(max_pages) + ' pages)')
 		sys.stdout.flush()
 
@@ -162,8 +172,8 @@ def get_bookmarks(url, header_info):
 			bookmarks += get_users(tags, 'bookmark')
 
 			# next page
-			count+=1
-			req = requests.get(url+'?page='+str(count), headers=headers)
+			count += 1
+			req = requests.get(url + '?page=' + str(count), headers=headers)
 			src = req.text
 			soup = BeautifulSoup(src, 'html.parser')
 			sys.stdout.write('.')
@@ -176,37 +186,39 @@ def get_bookmarks(url, header_info):
 	print('')
 	return bookmarks
 
+
 # get users from bookmarks/kudos
-def get_users (meta, kind):
+def get_users(meta, kind):
 	users = []
 
 	if meta != None:
 		for tag in meta:
-				if type(tag).__name__ == 'Tag':
-					if kind == 'kudo':
-						user = tag.contents[0]
-					elif kind == 'bookmark':
-						username = tag.find('h5', class_='byline heading').findChildren('a')[0].contents[0]
-						datetime = tag.findChildren('p', class_='datetime')[0].contents[0]
+			if type(tag).__name__ == 'Tag':
+				if kind == 'kudo':
+					user = tag.contents[0]
+				elif kind == 'bookmark':
+					username = tag.find('h5', class_='byline heading').findChildren('a')[0].contents[0]
+					datetime = tag.findChildren('p', class_='datetime')[0].contents[0]
 
-						bookmarkTags = tag.findAll('a', class_='tag')
-						bookmarkTags = [item.contents[0] for item in bookmarkTags]
+					bookmarkTags = tag.findAll('a', class_='tag')
+					bookmarkTags = [item.contents[0] for item in bookmarkTags]
 
-						collections = tag.find('ul', class_='meta commas')
-						if collections != None:
-							collections = [item.contents[0] for item in collections.findAll('a')]
-						
-						summary = tag.find('blockquote', class_='userstuff summary')
-						if summary != None: 
-							summary = summary.findAll('p')
-							summary = [item.contents[0] for item in summary]
+					collections = tag.find('ul', class_='meta commas')
+					if collections != None:
+						collections = [item.contents[0] for item in collections.findAll('a')]
 
-						user = { 'username': username, 'date': datetime, 'tags': bookmarkTags, \
+					summary = tag.find('blockquote', class_='userstuff summary')
+					if summary != None:
+						summary = summary.findAll('p')
+						summary = [item.contents[0] for item in summary]
+
+					user = {'username': username, 'date': datetime, 'tags': bookmarkTags, \
 							'collections': collections, 'summary': summary}
 
-					users.append(user)
+				users.append(user)
 
 	return users
+
 
 def get_single_comment(comment, parent):
 	# Find username
@@ -230,7 +242,7 @@ def get_single_comment(comment, parent):
 	commentid = comment['id'].split('_')[1]
 
 	# Get direct comment text
-	text = comment.find('blockquote', class_ ='userstuff')#.findAll('p').contents[0]
+	text = comment.find('blockquote', class_='userstuff')  # .findAll('p').contents[0]
 	text = text.findAll('p')
 	text = [item.contents[0] for item in text]
 	fullText = ''
@@ -242,8 +254,9 @@ def get_single_comment(comment, parent):
 			fullText += item
 
 	# Create object and return
-	commentData = {'user': username, 'datetime': dateObj, 'id': commentid, 'parent': parent, 'text': fullText}
+	commentData = {'user': username, 'datetime': dateObj, 'id': commentid, 'parent': parent, 'text': unidecode(fullText)}
 	return commentData
+
 
 def get_comment_thread(comment_thread, parent):
 	all_comments = []
@@ -251,7 +264,7 @@ def get_comment_thread(comment_thread, parent):
 	# Recurse till you find the deepest level
 	try:
 		nest_level = comment_thread[0]
-		nest_level = nest_level.findChild('ol', class_ ='thread')[0]
+		nest_level = nest_level.findChild('ol', class_='thread')[0]
 
 		new_comments = get_comment_thread(comment_thread, False)
 		all_comments.append(new_comments)
@@ -262,22 +275,23 @@ def get_comment_thread(comment_thread, parent):
 
 		# extracts all comments in line thread
 		for c, comment in enumerate(comments):
-				try:
-					if comment.attrs['class']:
-						if 'odd' in comment.attrs['class'] or 'even' in comment.attrs['class']:
-							single_comment = get_single_comment(comment, False)
-							all_comments.append(single_comment)
-							
-				except:
-					pass
-    
+			try:
+				if comment.attrs['class']:
+					if 'odd' in comment.attrs['class'] or 'even' in comment.attrs['class']:
+						single_comment = get_single_comment(comment, False)
+						all_comments.append(single_comment)
+
+			except:
+				pass
+
 		return all_comments
 
 	return all_comments
 
+
 def get_comments(url, header_info):
 	all_comments = []
-	headers = {'user-agent' : header_info}
+	headers = {'user-agent': header_info}
 
 	req = requests.get(url, headers=headers)
 	src = req.text
@@ -285,12 +299,12 @@ def get_comments(url, header_info):
 
 	# find all pages
 	if (soup.find('ol', class_='pagination actions')):
-		pages = soup.find('ol', class_='pagination actions').findChildren("li" , recursive=False)
+		pages = soup.find('ol', class_='pagination actions').findChildren("li", recursive=False)
 		max_pages = int(pages[-2].contents[0].contents[0])
 		count = 1
 
 		while count <= max_pages:
-			comments = soup.find('ol', class_ = 'thread').findChildren("li" , recursive=False)
+			comments = soup.find('ol', class_='thread').findChildren("li", recursive=False)
 
 			# comments processing
 			for c, comment in enumerate(comments):
@@ -303,16 +317,16 @@ def get_comments(url, header_info):
 				# likely a comment thread
 				except:
 					if comment.findChild('ol', class_="thread"):
-						all_comments.append(get_comment_thread(comment.findChildren('ol'), True, all_comments))
+						all_comments.append(get_comment_thread(comment.findChildren('ol'), True))
 
 			# next page
-			count+=1
-			req = requests.get(url+'?page='+str(count), headers=headers)
+			count += 1
+			req = requests.get(url + '?page=' + str(count), headers=headers)
 			src = req.text
 			soup = BeautifulSoup(src, 'html.parser')
-			
+
 	else:
-		comments = soup.find('ol', class_ = 'thread').findChildren("li" , recursive=False)
+		comments = soup.find('ol', class_='thread').findChildren("li", recursive=False)
 		last_count = 0
 
 		# comments processing
@@ -322,33 +336,35 @@ def get_comments(url, header_info):
 					if 'odd' in comment.attrs['class'] or 'even' in comment.attrs['class']:
 						single_comment = get_single_comment(comment, True)
 						all_comments.append(single_comment)
-						last_count+=1
+						last_count += 1
 
 			# likely a comment thread
 			except:
 				if comment.findChild('ol', class_="thread"):
 					if len(all_comments) > 0:
 						new_comments = get_comment_thread(comment.findChildren('ol'), True)
-						
+
 						# If it's a bunch of replies, store as reply to previous comment
 						if len(all_comments) == last_count:
 							all_comments[-1]['reply'] = new_comments
-							last_count+=1 # To move the index along so that you don't erase the comments
+							last_count += 1  # To move the index along so that you don't erase the comments
 						else:
 							all_comments.append(new_comments)
 					else:
 						new_comments = get_comment_thread(comment.findChildren('ol'), True)
 						all_comments.append(new_comments)
-						last_count +=1
+						last_count += 1
 
 	return all_comments
-	
+
+
 def access_denied(soup):
 	if (soup.find(class_="flash error")):
 		return True
 	if (not soup.find(class_="work meta group")):
 		return True
 	return False
+
 
 def write_fic_to_csv(fic_id, only_first_chap, lang, include_bookmarks, writer, errorwriter, header_info=''):
 	'''
@@ -359,15 +375,15 @@ def write_fic_to_csv(fic_id, only_first_chap, lang, include_bookmarks, writer, e
 	header_info should be the header info to encourage ethical scraping.
 	'''
 	print('Scraping ', fic_id)
-	url = 'http://archiveofourown.org/works/'+str(fic_id)+'?view_adult=true'
+	url = 'http://archiveofourown.org/works/' + str(fic_id) + '?view_adult=true'
 	if not only_first_chap:
 		url = url + '&amp;view_full_work=true'
-	headers = {'user-agent' : header_info}
+	headers = {'user-agent': header_info}
 	req = requests.get(url, headers=headers)
 	src = req.text
 	soup = BeautifulSoup(src, 'html.parser')
 
-	print (url)
+	print(url)
 	if (access_denied(soup)):
 		print('Access Denied')
 		error_row = [fic_id] + ['Access Denied']
@@ -379,35 +395,40 @@ def write_fic_to_csv(fic_id, only_first_chap, lang, include_bookmarks, writer, e
 		stats = get_stats(meta)
 		title = unidecode(soup.find("h2", class_="title heading").string).strip()
 
-		#get kudos
-		kudos_url = 'http://archiveofourown.org' + soup.find(id='kudos_more_link')['href']
-
+		# get kudos
+		try:
+			kudos_url = 'http://archiveofourown.org' + soup.find(id='kudos_more_link')['href']
+		except:
+			kudos_url = url
 		all_kudos = get_all_kudos(kudos_url, header_info)
-		
-		#get bookmarks
-		bookmark_url = 'http://archiveofourown.org/works/'+str(fic_id)+'/bookmarks'
+
+		# get bookmarks
+		bookmark_url = 'http://archiveofourown.org/works/' + str(fic_id) + '/bookmarks'
 		all_bookmarks = get_bookmarks(bookmark_url, header_info)
 
 		# get comments
-		comments_url  = 'http://archiveofourown.org/works/' + str(fic_id) + '?show_comments=true#comments'
+		comments_url = url + '&amp;show_comments=true#comments'
+		print(comments_url)
 		all_comments = get_comments(comments_url, header_info)
 
-		#get the fic itself
-		content = soup.find("div", id= "chapters")
+		# get the fic itself
+		content = soup.find("div", id="chapters")
 		chapters = content.select('p')
 		chaptertext = '\n\n'.join([unidecode(chapter.text) for chapter in chapters])
 
-		row = [fic_id] + [title] + [author] + list(map(lambda x: ', '.join(x), tags)) + stats + [all_kudos] + [all_bookmarks] + [all_comments] + [chaptertext]
+		row = [fic_id] + [title] + [author] + list(map(lambda x: ', '.join(x), tags)) + stats + [all_kudos] + [
+			all_bookmarks] + [all_comments] + [chaptertext]
 
 		try:
 			writer.writerow(row)
 		except:
 			print('Unexpected error: ', sys.exc_info()[0])
-			error_row = [fic_id] +  [sys.exc_info()[0]]
+			error_row = [fic_id] + [sys.exc_info()[0]]
 			errorwriter.writerow(error_row)
 		print('Done.')
 
-def get_args(): 
+
+def get_args():
 	parser = argparse.ArgumentParser(description='Scrape and save some fanfic, given their AO3 IDs.')
 	parser.add_argument(
 		'ids', metavar='IDS', nargs='+',
@@ -419,20 +440,20 @@ def get_args():
 		'--header', default='',
 		help='user http header')
 	parser.add_argument(
-		'--restart', default='', 
+		'--restart', default='',
 		help='work_id to start at from within a csv')
 	parser.add_argument(
-		'--firstchap', default='', 
+		'--firstchap', default='',
 		help='only retrieve first chapter of multichapter fics')
 	parser.add_argument(
-		'--lang', default='', 
+		'--lang', default='',
 		help='only retrieves fics of certain language (e.g English), make sure you use correct spelling and capitalization or this argument will not work')
 	parser.add_argument(
-		'--bookmarks', action='store_true', 
+		'--bookmarks', action='store_true',
 		help='retrieve bookmarks; ')
 	args = parser.parse_args()
 	fic_ids = args.ids
-	is_csv = (len(fic_ids) == 1 and '.csv' in fic_ids[0]) 
+	is_csv = (len(fic_ids) == 1 and '.csv' in fic_ids[0])
 	csv_out = str(args.csv)
 	headers = str(args.header)
 	restart = str(args.restart)
@@ -447,6 +468,7 @@ def get_args():
 		lang = False
 	return fic_ids, csv_out, headers, restart, is_csv, ofc, lang, include_bookmarks
 
+
 def process_id(fic_id, restart, found):
 	if found:
 		return True
@@ -455,6 +477,7 @@ def process_id(fic_id, restart, found):
 	else:
 		return False
 
+
 def main():
 	fic_ids, csv_out, headers, restart, is_csv, only_first_chap, lang, include_bookmarks = get_args()
 	os.chdir(os.getcwd())
@@ -462,10 +485,13 @@ def main():
 		writer = csv.writer(f_out)
 		with open("errors_" + csv_out, 'a') as e_out:
 			errorwriter = csv.writer(e_out)
-			#does the csv already exist? if not, let's write a header row.
+			# does the csv already exist? if not, let's write a header row.
 			if os.stat(csv_out).st_size == 0:
 				print('Writing a header row for the csv.')
-				header = ['work_id', 'title', 'author', 'rating', 'category', 'fandom', 'relationship', 'character', 'additional tags', 'language', 'published', 'status', 'status date', 'words', 'chapters', 'comments', 'kudos', 'bookmarks', 'hits', 'all_kudos', 'all_bookmarks', 'all_comments', 'body']
+				header = ['work_id', 'title', 'author', 'rating', 'category', 'fandom', 'relationship', 'character',
+						  'additional tags', 'language', 'published', 'status', 'status date', 'words', 'chapters',
+						  'comments', 'kudos', 'bookmarks', 'hits', 'all_kudos', 'all_bookmarks', 'all_comments',
+						  'body']
 				writer.writerow(header)
 			if is_csv:
 				csv_fname = fic_ids[0]
@@ -475,16 +501,18 @@ def main():
 						for row in reader:
 							if not row:
 								continue
-							write_fic_to_csv(row[0], only_first_chap, lang, include_bookmarks, writer, errorwriter, headers)
+							write_fic_to_csv(row[0], only_first_chap, lang, include_bookmarks, writer, errorwriter,
+											 headers)
 							time.sleep(delay)
-					else: 
+					else:
 						found_restart = False
 						for row in reader:
 							if not row:
 								continue
 							found_restart = process_id(row[0], restart, found_restart)
 							if found_restart:
-								write_fic_to_csv(row[0], only_first_chap, lang, include_bookmarks, writer, errorwriter, headers)
+								write_fic_to_csv(row[0], only_first_chap, lang, include_bookmarks, writer, errorwriter,
+												 headers)
 								time.sleep(delay)
 							else:
 								print('Skipping already processed fic')
@@ -493,5 +521,6 @@ def main():
 				for fic_id in fic_ids:
 					write_fic_to_csv(fic_id, only_first_chap, lang, include_bookmarks, writer, errorwriter, headers)
 					time.sleep(delay)
+
 
 main()
